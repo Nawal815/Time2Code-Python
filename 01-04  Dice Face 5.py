9c94e196965b1bf690792f61d94a32a027f8bfd8
Nawal815 <nawalamanullah08@gmail.com>
2025-08-21 17:03:51 +0100

    Add files via upload

---
+++ 01-04  Dice Face 5.py
@@ -0,0 +1,20 @@
+# Dice face 5 program
+
+# -----------------------
+# Subprograms
+# -----------------------
+# Output a dice face using ASCII text
+def output5():
+    print(" ooooooooooooo ")
+    print(" o           o ")
+    print(" o    # #    o ")
+    print(" o     #     o ")
+    print(" o    # #    o ")
+    print(" o           o ")
+    print(" ooooooooooooo ")
+
+
+# -----------------------
+# Main program
+# -----------------------
+output5()
